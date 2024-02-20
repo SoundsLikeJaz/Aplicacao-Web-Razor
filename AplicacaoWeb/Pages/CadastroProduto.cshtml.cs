f0dfe1fb839a85223051d932c7ba3df7f6121fcd
Jackson Silva <jacksonslv36@gmail.com>
2024-02-19 23:18:54 -0300

    Atualização

---
+++ AplicacaoWeb/Pages/CadastroProduto.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AplicacaoWeb.Pages
+{
+    public class CadastroProdutoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
